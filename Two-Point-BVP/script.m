ae3567f184668828487a248bd19c89683e675ae9
Ca11me1ce <898355011cy@gmail.com>
2018-08-07 23:22:34 -0400

    Updates

---
+++ Two-Point-BVP/script.m
@@ -0,0 +1,18 @@
+a=0;
+b=pi/2;
+N=10;
+k=-0.3;
+j=-0.1;
+[e,U1]=two_point(a,b,k,j,'m',N);
+
+plot(e,U1);
+hold
+r=zeros(N-1,1);
+for i=1:N-1,
+r(i)=-(sin(e(i))+3*cos((e(i))))/10;
+end
+
+plot(e,r)
+figure(2);
+plot(e,U1-r)
+norm(U1-r,inf) 
